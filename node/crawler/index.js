4e2e87e9a1bb391208446549fd9d004309c9b9be
bobolovecat <youngmaple@qq.com>
2024-01-29 17:13:31 +0800

    feat(crawler): 选择器

--- node/crawler/index.js
+++ node/crawler/index.js
@@ -8,11 +8,24 @@ const puppeteer = require('puppeteer-core');
 
     try {
         const page = await browser.newPage()
-        await page.goto('https://www.youtube.com/watch?v=qo_fUjb02ns', {
+        await page.goto('https://www.youtube.com/watch?v=m0lztaRC8FQ', {
             waitUtil: 'networkidle2'
         })
+        await page.waitForTimeout(1000)
         // await page.screenshot({ path: "screenshot.png" })
-        await page.pdf({ path: 'youtube.pdf' })
+        // await page.pdf({ path: 'youtube.pdf' })
+        // const input = await page.$('input#search')
+        // input.type('球球的前端奶茶屋')
+        // const html = await page.evaluate(() =>
+        //     document.body.innerHTML
+        // )
+        const like = await page.$$eval('like-button-view-model .yt-spec-button-shape-next__button-text-content', divs => {
+            return divs[0].textContent;
+        })
+        const view = await page.$$eval('#description-inner #info span', divs => {
+            return divs[0].textContent;
+        })
+        console.log(like, view)
     } catch (e) {
         console.error(e)
     } finally {
