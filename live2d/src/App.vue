3260096e730d057793cfe45a58346be4bb158ba4
bobolovecat <youngmaple@qq.com>
2024-08-02 18:07:12 +0800

    feat(live2d): update model

--- live2d/src/App.vue
+++ live2d/src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onBeforeMount, onMounted, ref } from 'vue'
 import axios from 'axios'
 import * as PIXI from 'pixi.js'
 import {
   Live2DModel,
   MotionPreloadStrategy,
-  InternalModel,
 } from 'pixi-live2d-display';
 
 window.PIXI = PIXI;
@@ -19,53 +18,17 @@ let model;
 
 // 嘴把张开闭合逻辑
 const setMouthOpenY = (v) => {
-  v = Math.max(0,Math.min(1,v));
+  v = Math.max(0, Math.min(1, v));
   // model.internalModel.coreModel.setParameterValueById('PARAM_MOUTH_FORM',v);
   model.internalModel.coreModel.setParamFloat('PARAM_MOUTH_OPEN_Y', v);
 }
 
-function init() {
-  (async function() {
-    // model = await Live2DModel.from('../../src/assets/Mao/Mao.model3.json', { motionPreload: MotionPreloadStrategy.NONE,  })
-    model = await Live2DModel.from('../../src/assets/mikoto/mikoto.model.json', { motionPreload: MotionPreloadStrategy.NONE,  })
-    app = new PIXI.Application({
-      view: canvas.value,
-      height: window.innerHeight,
-      width: window.innerWidth,
-      resolution: window.devicePixelRatio,
-      autoDensity: true,
-      autoResize: true,
-      antialias: true,
-      transparent: true,
-      autoUpdate: true,
-    });
-    app.stage.addChild(model)
-    // 模型的位置,x,y相较于窗口左上角
-    model.x = 0
-    model.y = -200
-
-    model.on('pointerdown', (hitAreas) => {   
-      const { x, y } = hitAreas.data.global  
-    // 需要用到频谱的时候 从分析仪获取到 之前备用的 frequencyData 里
-      model.motion('tap', 5)
-      const point = model.hitTest(x, y)
-      console.log(point);
-    });
-  })()
-}
-
-function addFrame(model) {
-  const foreground = PIXI.Sprite.from(PIXI.Texture.WHITE);
-  foreground.width = model.internalModel.width;
-  foreground.height = model.internalModel.height;
-  foreground.alpha = 0.2;
-
-  model.addChild(foreground);
-  foreground.visible = true
+function random(min, max) { 
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function speaking() {
-  (async function() {
+  (async function () {
 
     // hitAreas:模型的一些上下文
 
@@ -81,7 +44,7 @@ function speaking() {
           responseType: 'arraybuffer'
         })
         const audioData = response.data;
-        audioCtx.decodeAudioData(audioData, function(buffer) {
+        audioCtx.decodeAudioData(audioData, function (buffer) {
           // 新建 Buffer 源
           const source = audioCtx.createBufferSource();
           source.buffer = buffer;
@@ -92,7 +55,7 @@ function speaking() {
           // 开始播放
           source.start();
 
-          source.onended = () =>{
+          source.onended = () => {
             playing = false;
             setMouthOpenY(0);
           };
@@ -100,16 +63,16 @@ function speaking() {
       }
 
       fetchAudioData(audioLink)
-      const getByteFrequencyData = ()=>{
+      const getByteFrequencyData = () => {
         analyser.getByteFrequencyData(frequencyData);
         return frequencyData;
       };
 
       const o = 80;
-      const arrayAdd = (a) => a.reduce((i,a) => i+a, 0);
+      const arrayAdd = (a) => a.reduce((i, a) => i + a, 0);
       let playing = true;
-      const run = () =>{
-        if(!playing) return;
+      const run = () => {
+        if (!playing) return;
         console.log(playing);
         const frequencyData = getByteFrequencyData();
         const arr = [];
@@ -117,8 +80,8 @@ function speaking() {
         for (var i = 0; i < 700; i += o) {
           arr.push(frequencyData[i]);
         }
-        setMouthOpenY((arrayAdd(arr)/arr.length - 20)/60);
-        setTimeout(run,1000/30);
+        setMouthOpenY((arrayAdd(arr) / arr.length - 20) / 60);
+        setTimeout(run, 1000 / 30);
       }
       run();
     }
@@ -127,29 +90,46 @@ function speaking() {
 }
 
 function animation() {
-  (async function() {
-    model.motion('motion5')
-  })()
+  model.motion(`motion${random(0, 12)}`)
+}
+function expression() {
+  model.expression(`f0${random(1, 8)}`)
 }
 
-onMounted(() => {
-  // addFrame(model)
-  init()
+onMounted(async () => {
+  app = new PIXI.Application({
+    view: canvas.value,
+    height: window.innerHeight,
+    width: window.innerWidth,
+    autoDensity: true,
+    antialias: true,
+    resolution: window.devicePixelRatio,
+    // transparent: true,
+  });
+  model = await Live2DModel.from('../../src/assets/mikoto/mikoto.model.json')
+  app.stage.addChild(model)
+  model.scale.set(.6)
+})
+
+onBeforeUnmount(() => {
+  model?.destroy()
+  app?.destroy()
 })
 </script>
-  
+
 <template>
   <div style="width:100%;height:100%;z-index: -1000;">
-    <canvas ref="canvas" class="canvas_view_content" id="canvas_view"></canvas>
-    <div class="btn speaking" @click="$event => speaking()">说话</div>
+    <canvas ref="canvas"></canvas>
     <div class="btn animation" @click="$event => animation()">动画</div>
+    <div class="btn expression" @click="$event => expression()">表情</div>
+    <div class="btn speaking" @click="$event => speaking()">说话</div>
   </div>
 </template>
-  
+
 <style scoped>
 .btn {
   position: fixed;
-  top:30px;
+  top: 30px;
   left: 30px;
   width: 100px;
   height: 60px;
@@ -161,7 +141,12 @@ onMounted(() => {
   background: #000;
   z-index: 5;
 }
-.animation {
+
+.expression {
   top: 100px;
+}
+
+.speaking {
+  top: 170px;
 }
 </style>
