26b92f80f3d6d87dd1238427539b3f9ece1f310b
bobolovecat <youngmaple@qq.com>
2024-08-07 11:32:30 +0800

    refactor(live2d): speaking

--- live2d/src/App.vue
+++ live2d/src/App.vue
@@ -1,92 +1,50 @@
 <script setup>
-import { onBeforeMount, onMounted, ref } from 'vue'
-import axios from 'axios'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import * as PIXI from 'pixi.js'
-import {
-  Live2DModel,
-  MotionPreloadStrategy,
-} from 'pixi-live2d-display';
+import { Live2DModel } from 'pixi-live2d-display';
 
 window.PIXI = PIXI;
 
 const canvas = ref(null)
-
-const audioUrl = ref('https://image-1251917893.file.myqcloud.com/tip-project/live2d/audio/out3.ogg');
+const audioFile = 'https://image-1251917893.file.myqcloud.com/tip-project/live2d/audio/out3.ogg';
 
 let app;
 let model;
+let audioContext;
 
-// 嘴把张开闭合逻辑
-const setMouthOpenY = (v) => {
-  v = Math.max(0, Math.min(1, v));
-  // model.internalModel.coreModel.setParameterValueById('PARAM_MOUTH_FORM',v);
-  model.internalModel.coreModel.setParamFloat('PARAM_MOUTH_OPEN_Y', v);
-}
-
-function random(min, max) { 
+function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function speaking() {
-  (async function () {
+async function speaking() {
+  const response = await fetch(audioFile);
+  const audioData = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(audioData);
 
-    // hitAreas:模型的一些上下文
+  const source = audioContext.createBufferSource();
+  const analyser = audioContext.createAnalyser();
 
-    function audioPlay(audioLink) {
-      const audioCtx = new AudioContext();
-      // 新建分析仪
-      const analyser = audioCtx.createAnalyser();
-      // 根据 频率分辨率建立个 Uint8Array 数组备用
-      const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-      // 取音频文件成 arraybuffer
-      async function fetchAudioData(audioOrigin) {
-        const response = await axios.get(audioOrigin, {
-          responseType: 'arraybuffer'
-        })
-        const audioData = response.data;
-        audioCtx.decodeAudioData(audioData, function (buffer) {
-          // 新建 Buffer 源
-          const source = audioCtx.createBufferSource();
-          source.buffer = buffer;
-          // 连接到 audioCtx
-          source.connect(audioCtx.destination);
-          // 连接到 音频分析器
-          source.connect(analyser);
-          // 开始播放
-          source.start();
+  source.buffer = audioBuffer;
+  analyser.connect(audioContext.destination);
+  source.connect(analyser);
 
-          source.onended = () => {
-            playing = false;
-            setMouthOpenY(0);
-          };
-        });
-      }
+  source.start();
 
-      fetchAudioData(audioLink)
-      const getByteFrequencyData = () => {
-        analyser.getByteFrequencyData(frequencyData);
-        return frequencyData;
-      };
+  const updateMouth = () => {
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
 
-      const o = 80;
-      const arrayAdd = (a) => a.reduce((i, a) => i + a, 0);
-      let playing = true;
-      const run = () => {
-        if (!playing) return;
-        console.log(playing);
-        const frequencyData = getByteFrequencyData();
-        const arr = [];
-        // 频率范围还是太广了，跳采！
-        for (var i = 0; i < 700; i += o) {
-          arr.push(frequencyData[i]);
-        }
-        setMouthOpenY((arrayAdd(arr) / arr.length - 20) / 60);
-        setTimeout(run, 1000 / 30);
-      }
-      run();
+    const volume = dataArray.reduce((a, b) => a + b) / dataArray.length;
+    const mouthOpen = Math.min(1, volume / 50);
+
+    model.internalModel.coreModel.setParamFloat('PARAM_MOUTH_OPEN_Y', mouthOpen);
+
+    if (audioContext.state !== 'closed') {
+      requestAnimationFrame(updateMouth);
     }
-    audioPlay(audioUrl.value);
-  })()
+  };
+
+  updateMouth();
 }
 
 function animation() {
@@ -97,6 +55,7 @@ function expression() {
 }
 
 onMounted(async () => {
+  audioContext = new AudioContext()
   app = new PIXI.Application({
     view: canvas.value,
     height: window.innerHeight,
@@ -114,6 +73,7 @@ onMounted(async () => {
 onBeforeUnmount(() => {
   model?.destroy()
   app?.destroy()
+  audioContext?.close()
 })
 </script>
 
