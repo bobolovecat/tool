747108ab034e4b0f4545dfd17ff0d22ef3fbe419
bobolovecat <youngmaple@qq.com>
2024-09-14 15:53:04 +0800

    feat: spine

---
+++ spine/js/index.js
@@ -0,0 +1,8 @@
+new spine.SpinePlayer("player-container", {
+    jsonUrl: "/assets/genshin/1.json",
+    atlasUrl: "/assets/genshin/1.atlas",
+    // showControls : false,
+    // animation: 'animation',
+    // alpha : true , // 启用播放器半透明
+    // backgroundColor : "#00000000",  // 背景完全透明
+});
